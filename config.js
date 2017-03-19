a9551702c0b2b1e082aea80a11a2585b20b98275
thithy19 <kthiepthy@gmail.com>
2017-03-19 22:05:22 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+  consumer_key:         'FLYg4eiqBftuwwoyErkOaUTfH',
+  consumer_secret:      'pS4sUVRYMgvWGKzAm23uG7V1MSJcnz1W28JXF5XMh28IckX4e2',
+  access_token:         '791549427026628610-QeDDvtDDYvtubbiudY7Sb3KTXToH1rp',
+  access_token_secret:  'piScH2YAUEWUpzkEZc7TAFYvIen8bdewQmSUMoYyn7Cwq',
+
+}
