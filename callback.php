6aaf66c4079fad2df16cac1d08ab0eb228ed926a
thithy19 <kthiepthy@gmail.com>
2017-04-18 14:35:41 +0200

    Update callback.php

--- callback.php
+++ callback.php
@@ -21,7 +21,7 @@
 					<div class="pull-right">
 						<?php
 						session_start();
-						require('twitteroauth/twitteroauth.php');
+						require('twitteroauth.php');
 						$connection = new TwitterOAuth('4JPhVqb8l96ViuOlQaDKQK85h', 'E7chGojc5ZwNfAEHeI5BQDtk5xCKHL7N7GAl4QBM6Anfa6mIYR', $_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
 						$access_token = $connection->getAccessToken($_REQUEST['oauth_verifier']);
 
