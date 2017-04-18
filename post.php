5fb49f554a3d1c4f323122b208ee43e3ce8a32b6
thithy19 <kthiepthy@gmail.com>
2017-04-18 14:34:30 +0200

    Update post.php

--- post.php
+++ post.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-require('twitteroauth/twitteroauth.php');
+require('twitteroauth.php');
 
 $connection = new TwitterOAuth('4JPhVqb8l96ViuOlQaDKQK85h', 'E7chGojc5ZwNfAEHeI5BQDtk5xCKHL7N7GAl4QBM6Anfa6mIYR', $_SESSION['def_oauth_token'], $_SESSION['def_oauth_token_secret']);
 
